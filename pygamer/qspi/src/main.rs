b33d550a3b1638d23e907a0a9bfddee2a4403024
Dan Whitman <dwhitman44@gmail.com>
2025-11-25 15:56:29 -0500

    Transfer. Adds `qspi` the Pygamer tests to test the `pygamer` BSP `qspi` example so that we can see panic messages.

--- pygamer/qspi/src/main.rs
+++ pygamer/qspi/src/main.rs
@@ -5,15 +5,98 @@
 
 use bsp::{Pins, RedLed};
 use hal::{
-    clock::v2::clock_system_at_reset,
     delay::Delay,
     prelude::*,
-    qspi::{self, Command, QspiBuilder},
+    qspi::{self, Command},
 };
 use shared_pygamer::prelude::*;
 
 #[entry]
 fn main() -> ! {
+    let (mut delay, mut red_led) = example();
+
+    loop {
+        red_led.toggle();
+        delay.delay_ms(1000u16);
+    }
+}
+
+#[cfg(feature = "clock-v1")]
+fn example() -> (Delay, RedLed) {
+    use hal::clock::GenericClockController;
+
+    let mut peripherals = Peripherals::take().unwrap();
+    let core = CorePeripherals::take().unwrap();
+    let mut clocks = GenericClockController::with_internal_32kosc(
+        peripherals.gclk,
+        &mut peripherals.mclk,
+        &mut peripherals.osc32kctrl,
+        &mut peripherals.oscctrl,
+        &mut peripherals.nvmctrl,
+    );
+    let mut delay = Delay::new(core.SYST, &mut clocks);
+
+    let sets = Pins::new(peripherals.port).split();
+
+    let mut flash = sets.flash.init(&mut peripherals.mclk, peripherals.qspi);
+
+    // Startup delay. Can't find documented but Adafruit use 5ms
+    delay.delay_ms(5u8);
+    // Reset. It is recommended to check the BUSY(WIP?) bit and the SUS before reset
+    wait_ready(&mut flash);
+    flash.run_command(Command::EnableReset).unwrap();
+    flash.run_command(Command::Reset).unwrap();
+    // tRST(30μs) to reset. During this period, no command will be accepted
+    delay.delay_ms(1u8);
+
+    // Check for GD25Q64C JEDEC ID
+    let mut read_buf = [0u8; 3];
+    flash.read_command(Command::ReadId, &mut read_buf).unwrap();
+    assert_eq!(read_buf, [0x17, 0x40, 0xc8]);
+
+    // 120MHz / 2 = 60mhz
+    // faster than 104mhz at 3.3v would require High Performance Mode
+    flash.set_clk_divider(2);
+
+    // Enable Quad SPI mode. Requires write enable. Check WIP.
+    flash.run_command(Command::WriteEnable).unwrap();
+    flash.write_command(Command::WriteStatus2, &[0x02]).unwrap();
+    wait_ready(&mut flash);
+
+    // Chip Erase. Requires write enable. Check WIP.
+    flash.run_command(Command::WriteEnable).unwrap();
+    flash.erase_command(Command::EraseChip, 0x0).unwrap();
+    // Worst case up to 140 seconds!
+    wait_ready(&mut flash);
+
+    // Page Program. Requires write enable. Check WIP.
+    // If more than 256 bytes are sent to the device, previously latched data
+    // are discarded and the last 256 data bytes are guaranteed to be
+    // programmed correctly within the same page. If less than 256 data
+    // bytes are sent to device, they are correctly programmed at the
+    // requested addresses without having any effects on the other bytes of
+    // the same page
+
+    let write_buf = [0x0d, 0xd0, 0x01, 0xc0];
+    flash.run_command(Command::WriteEnable).unwrap();
+    flash.write_memory(0, &write_buf);
+    wait_ready(&mut flash);
+
+    // Read back data
+    // datasheet claims 6BH needs a single dummy byte, but doesnt work then
+    // adafruit uses 8, and the underlying implementation uses 8 atm as well
+    let mut read_buf = [0u8; 4];
+    flash.read_memory(0, &mut read_buf);
+    assert_eq!(read_buf, write_buf);
+
+    // END OF ACTUAL EXAMPLE
+    (delay, sets.led_pin.into())
+}
+
+#[cfg(not(feature = "clock-v1"))]
+fn example() {
+    use hal::{clock::v2::clock_system_at_reset, qspi::QspiBuilder};
+
     let mut peripherals = Peripherals::take().unwrap();
     let core = CorePeripherals::take().unwrap();
 
@@ -29,8 +112,6 @@ fn main() -> ! {
 
     let sets = Pins::new(peripherals.port).split();
 
-    let mut red_led: RedLed = sets.led_pin.into();
-
     let apb_qspi = clocks.apbs.qspi;
     let ahb_qspi = clocks.ahbs.qspi;
 
@@ -42,8 +123,10 @@ fn main() -> ! {
         sets.flash.data2,
         sets.flash.data3,
     )
-    // 48Mhz since this is as fast as the CPU runs after reset,
-    .with_freq(48_000_000)
+    // QSPI freq can never be more than 1/2 of the CPU freq.
+    // CPU is running at 48Mhz by default, so max QSPI speed
+    // like this is 24Mhz
+    .with_freq(24_000_000)
     .with_mode(qspi::QspiMode::_0)
     .build(peripherals.qspi, ahb_qspi, apb_qspi, gclk0)
     .unwrap();
@@ -93,10 +176,8 @@ fn main() -> ! {
     flash.read_memory(0, &mut read_buf);
     assert_eq!(read_buf, write_buf);
 
-    loop {
-        red_led.toggle();
-        delay.delay_ms(1000u16);
-    }
+    /// END OF ACTUAL EXAMPLE
+    sets.led_pin.into()
 }
 
 /// Wait for the write-in-progress and suspended write/erase.
